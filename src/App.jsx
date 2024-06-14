3f68d91fd96e5f48abbfff7fc20ea643d92b4145
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-06-14 12:21:45 +0530

    install: tailwind css , remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import './App.css'
 
 function App() {
   const [socket, setSocket] = useState(null);
@@ -8,7 +7,7 @@ function App() {
   const [msgs, setmsgs] = useState([]);
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:8000/chat');
+    const socket = new WebSocket(`ws://localhost:8000/chat?userId=${window.localStorage.getItem("biscut")}`)
     socket.onopen = () => {
       console.log('Connected')
       setSocket(socket);
@@ -23,11 +22,13 @@ function App() {
   }, [])
 
   const handleSendMSG = () => {
+    const connections = JSON.parse(window.localStorage.getItem("connections"));
     if (newMsg != "") {
       const data = JSON.stringify({
         content: newMsg,
         sender_id: window.localStorage.getItem("biscut"),
-        receiver_id: window.localStorage.getItem("connections")
+        receiver_id: connections[0],
+        state: 'pending',
       });
       socket.send(data);
       setNewMsg("")
@@ -42,18 +43,18 @@ function App() {
       name: e.target.name.value,
       email: e.target.email.value,
       password: e.target.password.value,
-      status: 'online',
-      connections: ['66641d65c4143d51b00480ed']
-    }
-
-    axios.post('http://localhost:8000/user/signup', user)
-    .then(function (response) {
-      window.localStorage.setItem("biscut", response?.data?._id);
-      window.localStorage.setItem("connections", response?.data?.connections);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      status: "online",
+      connections: ["66641d65c4143d51b00480ed", "666569a036995de3965f84bb"]
+      }
+      
+      axios.post('http://localhost:8000/user/signup', user)
+      .then(function (response) {
+        window.localStorage.setItem("biscut", response?.data?._id);
+        window.localStorage.setItem("connections", JSON.stringify(response?.data?.connections));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   if (!socket) {
@@ -71,20 +72,21 @@ function App() {
     )
   })
   return (
-    <>
+    <div className='bg-gray-900 w-screen h-screen'>
       <form onSubmit={handleSubmit}>
-        <input name="username" type="text" placeholder='USERNAME'/>
-        <input name="name" type="text" placeholder='NAME'/>
-        <input name="email" type="text" placeholder='EMAIL'/>
-        <input name="password" type="text" placeholder='PASSWORD'/>
+        <input name="username" type="text" placeholder='USERNAME' />
+        <input name="name" type="text" placeholder='NAME' />
+        <input name="email" type="text" placeholder='EMAIL' />
+        <input name="password" type="text" placeholder='PASSWORD' />
         <button>JOIN</button>
       </form>
       <div>
+        <div></div>
         {content}
         <input type="text" onChange={(e) => setNewMsg(e.target.value)} value={newMsg} />
         <button onClick={handleSendMSG}>Send</button>
       </div>
-    </>
+    </div>
   )
 }
 
