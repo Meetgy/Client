fb8c76b129fcae5c4cc940f9812b6faa33f04d3b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-07-10 05:23:14 +0530

    feat(reduc api): create Api for Login This commit create Singup Mutation for user Sign up inside loginApi and setup the redux api and export it from index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { connectionsApi } from "./apis/connectionsApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { loginApi } from "./apis/loginApi";
 
 export const store = configureStore({
     reducer:{
         [connectionsApi.reducerPath]: connectionsApi.reducer,        
+        [loginApi.reducerPath]: loginApi.reducer,        
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
                 .concat(connectionsApi.middleware)
+                .concat(loginApi.middleware)
     }
 })
 
 setupListeners(store.dispatch);
 
 export { useFetchUsersQuery } from "./apis/connectionsApi"
+export { useSignupMutation } from "./apis/loginApi"
