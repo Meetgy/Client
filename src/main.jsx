809e4837e9709ebc71a4a514261e285c0f4b09d4
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-07-10 21:40:04 +0530

    refactor(App comp): Refactor App comp Setup React-Router in main comp set two route one '/login' for Login page and another one '/' for App page which will eventually render all the comps from App.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store/index.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store/index.js';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  } from 'react-router-dom';
+import App from './App.jsx';
+import LoginPage from './pages/Login.jsx';
+
+
+if (!window.localStorage.getItem("biscut")) {
+  window.history.pushState({}, '', "/login");
+} else {
+  window.history.pushState({}, '', "/");
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
