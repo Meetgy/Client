a1708356c6668fdd51f019537bd07c37826f457a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-07-15 05:22:19 +0530

    feat(Input.jsx): Add dynamic placeholder behavior and error handling to the input field When the input is focused, the placeholder text shrinks. If there's an error, the placeholder text turns red and displays the error message. The input field also updates its value state on change and handles autofill styles.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const Input = ({ data }) => {
+const Input = ({ data, ErrorObject }) => {
     const [value, setValue] = useState("")
     const [placeholder, setPlaceholder] = useState(false);
 
@@ -11,12 +11,18 @@ const Input = ({ data }) => {
         setPlaceholder(false);
     }
 
+    useEffect(() => {
+        if (value !== '') {
+            setPlaceholder(true);
+        }
+    },[placeholder])
+
     return (
         <div className="flex flex-row w-112 h-12 m-2 bg-zinc-900 border-l-[3px] border-l-violet-500 justify-center">
-            <div className={`mt-0.5 flex flex-col w-full justify-between items-start text-xs px-1 text-gray-500 `}>
-                <div className={`pl-1 transition-all duration-300 ease-in-out  ${placeholder ? 'text-xs' : 'text-base'}`}>
-                    {placeholder ? data.placeholder : ' '}
-                </div>
+            <div className={`mt-0.5 flex flex-col w-full justify-between items-start text-xs px-1 text-gray-500 `}> 
+                    <div className={`pl-1 transition-all duration-300 ease-in-out  ${placeholder ? 'text-xs' : 'text-base'} ${ErrorObject && 'text-red-500 text-xs'}`}>
+                        {ErrorObject ? ErrorObject.message : placeholder ? data.placeholder : ' '}
+                    </div>
                 <input
                     id="auto-input-styles"
                     className="pl-1 w-full h-12 text-base flex-1 bg-zinc-900 select-none outline-none text-violet-400 caret-violet-400 auto-input-styles"
@@ -24,6 +30,7 @@ const Input = ({ data }) => {
                     type="text"
                     minLength={data.minlength}
                     maxLength={data.maxlength}
+                    required={data.required}
                     onChange={(e) => setValue(e.target.value)}
                     onFocus={onFocused}
                     onBlur={onBlured}
@@ -33,15 +40,6 @@ const Input = ({ data }) => {
             <div className="w-12 h-4 text-xs px-0.5 mt-0.5 text-gray-500">
                 {data.maxlength && <div className="w-12">{value.length} / {data.maxlength}</div>}
             </div>
-            <style>
-                {`
-                    auto-input-styles:: -webkit-autofill {
-                        -webkit-text-fill-color: white !important;
-                        -webkit-box-shadow: 0 0 0px 1000px #333 !important;
-                        background-color: #333 !important;
-                    }
-                `}
-            </style>
         </div>
     )
 }
