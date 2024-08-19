09cda2d52a080d3dacb84d9c036070da672fd73f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-08-19 14:04:57 +0530

    Refactor: refactorize the Chat component - Implement a receiver prop - Remove Connections.jsx component from Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
-import Connections from "./Connections";
 import { TiMessageTyping } from "react-icons/ti";
-import { useFetchUsersQuery } from "../store";
 
-const Chat = ({ socket, msgs }) => {
-    const [receiver, setReceiver] = useState(null);
+const Chat = ({ socket, msgs, receiver }) => {
     const [newMsg, setNewMsg] = useState("");
-
-    const {data, isError, isSuccess} = useFetchUsersQuery();
 
     const handleSendMSG = () => {
         if (newMsg != "") {
@@ -26,15 +21,6 @@ const Chat = ({ socket, msgs }) => {
         }
     };
 
-    const handleReceiverId = (user) => {
-        setReceiver(user)
-    }
-
-    let users;
-    if (!isError && isSuccess) {
-        users = <Connections data={data} handleReceiverId={handleReceiverId} />
-    }
-
     const content = msgs?.map((msg, i) => {
         return (
             <div key={i} className="text-white flex flex-row items-center">
@@ -45,29 +31,24 @@ const Chat = ({ socket, msgs }) => {
 
     return (
         <div className="flex flex-row items-center m-1">
-            <div>
-                {users}
+            <div className="text-gray-100 text-base gap-1 flex flex-row items-center">
+                <TiMessageTyping className="text-xl" />
+                {receiver?.name}
             </div>
-            <div>
-                <div className="text-gray-100 text-base gap-1 flex flex-row items-center">
-                    <TiMessageTyping className="text-xl" />
-                    {receiver?.name}
-                </div>
-                {content}
-                <input
-                    className="w-48 px-1 text-base m-2 bg-gray-100 rounded-md"
-                    type="text"
-                    onChange={(e) => setNewMsg(e.target.value)}
-                    value={newMsg}
-                    placeholder="Send Msg..."
-                />
-                <button
-                    className="px-2 bg-gray-300 text-base text-black rounded-md"
-                    onClick={handleSendMSG}
-                >
-                    Send
-                </button>
-            </div>
+            {content}
+            <input
+                className="w-48 px-1 text-base m-2 bg-gray-100 rounded-md"
+                type="text"
+                onChange={(e) => setNewMsg(e.target.value)}
+                value={newMsg}
+                placeholder="Send Msg..."
+            />
+            <button
+                className="px-2 bg-gray-300 text-base text-black rounded-md"
+                onClick={handleSendMSG}
+            >
+                Send
+            </button>
         </div>
     )
 }
