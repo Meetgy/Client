01acece1b961e9e35f24436339c76a53b71a5d63
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-09-15 12:06:59 +0530

    refactor(App.jsx): Refactor App.js and implement that code to Dashboard.jsx - Implement Route comp inside App.jsx and set paths for login and Dashboard page - Check that user is already logged in or not - Shipped the App.jsx's jsx code to Dashboard.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
-import Chat from "./components/Chat";
-import { Route, Routes } from "react-router-dom";
-import useWebSocket from "./hooks/useWebSocket.js"; // New custom hook
+import Route from "./components/utils/Route.jsx";
+import Login from "./pages/Login.jsx"
+import Dashboard from "./pages/Dashboard.jsx";
+import useNavigation from "./hooks/useNavigation.js";
 
 const App = () => {
-  const [msgs, setMsgs] = useState([]);
   const userId = window.localStorage.getItem("biscut");
-  const { socket, error } = useWebSocket(`ws://localhost:8000/chat?userId=${userId}`);
-
-  useEffect(() => {
-    if (socket) {
-      socket.onmessage = (msg) => {
-        const parsedMessage = JSON.parse(msg?.data);
-        if (parsedMessage) {
-          setMsgs(prevMsg => [...prevMsg, parsedMessage]);
-        }
-      };
-    }
-  }, [socket]);
-
-  if (error) {
-    return (
-      <div className="bg-[#212121] w-screen h-screen flex flex-col justify-center items-center text-white">
-        {`We're`} having trouble connecting right now. Please check your internet connection and try again later
-      </div>
-    );
+  const { navigate } = useNavigation();
+  
+  if (!userId) {
+    navigate("/login");
+  } else {
+    navigate("/dashboard");
   }
 
   return (
     <div className="bg-[#09090b] w-screen h-screen flex flex-col justify-center items-center">
-      <Routes>
-        <Route path={`/`} element={error || <Chat socket={socket} msgs={msgs} />} />
-      </Routes>
+      <Route path='/login'><Login /></Route>
+      <Route path='/dashboard'><Dashboard /></Route>
     </div>
   );
 }
