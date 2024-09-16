7cef4236c9866f05c4eb9a6534cf96e4b43df6ac
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-09-16 10:53:41 +0530

    feat: Implement onClick functionality to the Link.jsx component

--- src/components/utils/Link.jsx
+++ src/components/utils/Link.jsx
@@ -1,6 +1,6 @@
 import useNavigation from "../../hooks/useNavigation"
 
-const Link = ({ to, children, className }) => {
+const Link = ({ to, children, className, onClick }) => {
     const { navigate } = useNavigation();
     
     const handleClick = (e) => {
@@ -9,7 +9,10 @@ const Link = ({ to, children, className }) => {
         navigate(to)
     }
 
-    return <a href={to} onClick={handleClick} className={className}>{children}</a>
+    return <a href={to} onClick={(e) => {
+        handleClick(e)
+        onClick()
+    }} className={className}>{children}</a>
 }
 
 export default Link
