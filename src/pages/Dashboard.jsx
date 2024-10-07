8fb05fec5abf49c48b855349606a156b63255d00
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-10-07 08:07:51 +0530

    Update UI: Modify chat layout and styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -45,13 +45,15 @@ const Dashboard = () => {
     );
   }
   return (
-    <div className="bg-zinc-950 w-full h-full flex flex-col items-center">
-      <HorizontalBar />
-      <div className="flex flex-row justify-between h-screen w-screen">
+    <div className="bg-zinc-950 w-full h-screen flex flex-col">
+      <div className="sticky top-0 z-10">
+        <HorizontalBar />
+      </div>
+      <div className="flex flex-row justify-between flex-grow overflow-hidden">
         {users}
         <div className="bg-zinc-800 h-full w-3"></div>
-        <div className="flex flex-col justify-between w-full">
-          <div className="text-gray-100 text-lg flex flex-row items-center bg-[#1a1a1d] h-12 py-3">
+        <div className="flex flex-col justify-between w-full overflow-hidden">
+          <div className="text-gray-100 text-lg flex flex-row items-center bg-[#1a1a1d] h-12 py-3 sticky top-0 z-10">
             <div className="flex flex-row gap-2 items-center text-violet-500 px-4 text-lg"><MdKeyboardArrowLeft className="text-2xl" />{connection.name}</div>
           </div>
           <Route path={`/dashboard/${connection._id}`}>
