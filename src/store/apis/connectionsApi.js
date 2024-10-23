a0d235d16f95e5c7f0ce1f156510bd434a3319b5
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-10-23 10:14:05 +0530

    Add Comments

--- src/store/apis/connectionsApi.js
+++ src/store/apis/connectionsApi.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// Define the API slice
 const connectionsApi = createApi({
-    reducerPath: 'connections',
+    reducerPath: 'connections', // Unique key for the slice
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:8000"
-    }),
+    }), // Base URL for API requests
     endpoints: (builder) => {
         return {
             fetchUsers: builder.query({
