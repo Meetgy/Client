15382d08f5ae96e9ee2541dc8a799aef75b634c6
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-01-15 13:12:34 +0530

    update(getChatUsers.js): change file name to getChatUsersApi.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,20 @@ import { connectionsApi } from "./apis/connectionsApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { loginApi } from "./apis/loginApi";
 import { messagesReducer } from "./slices/messageSlice";
-import { getChatUsers } from "./apis/getChatUsers";
+import { getChatUsersApi } from "./apis/getChatUsersApi";
 
 export const store = configureStore({
     reducer: {
         messagesSlice: messagesReducer,
         [loginApi.reducerPath]: loginApi.reducer,
         [connectionsApi.reducerPath]: connectionsApi.reducer,
-        [getChatUsers.reducerPath]: getChatUsers.reducer,
+        [getChatUsersApi.reducerPath]: getChatUsersApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
             .concat(loginApi.middleware)
             .concat(connectionsApi.middleware)
-            .concat(getChatUsers.middleware)
+            .concat(getChatUsersApi.middleware)
     }
 })
 
@@ -24,7 +24,7 @@ setupListeners(store.dispatch);
 
 export { useSignupMutation, useLoginMutation } from "./apis/loginApi"
 export { useFetchUsersQuery } from "./apis/connectionsApi"
-export { useGetChatUsersQuery } from "./apis/getChatUsers"
+export { useGetChatUsersApiQuery } from "./apis/getChatUsersApi"
 export {
     oldMessages,
     addMessages,
