5c534c9734bb1b930331e4e632bcc1be4640aa29
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-01-26 23:49:38 +0530

    update: change data field name to chats for useChatUsers

--- src/hooks/useChatUsers.js
+++ src/hooks/useChatUsers.js
@@ -1,6 +1,8 @@
 import { useGetChatUsersQuery } from "../store/index";
 
-export const useChatUsers = () => {
+const useChatUsers = () => {
   const { data, isError, isSuccess } = useGetChatUsersQuery();
-  return { userss: data || [], usersIsError: isError, usersIsSuccess: isSuccess };
+  return { chats: data || [], isError, isSuccess };
 };
+
+export default useChatUsers;
